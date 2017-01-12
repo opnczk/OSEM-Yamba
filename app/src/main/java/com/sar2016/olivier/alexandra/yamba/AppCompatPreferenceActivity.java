e12912df789b2c3f0e805c93cffd33f428df1044
alexandraHospital <hospital.alex@gmail.com>
2017-01-12 19:58:57 +0100

    Settings with username + password, on PreferenceActivity for testings, it will be in status activity later

---
+++ app/src/main/java/com/sar2016/olivier/alexandra/yamba/AppCompatPreferenceActivity.java
@@ -0,0 +1,130 @@
+package com.sar2016.olivier.alexandra.yamba;
+
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+/**
+ * A {@link android.preference.PreferenceActivity} which implements and proxies the necessary calls
+ * to be used with AppCompat.
+ */
+public abstract class AppCompatPreferenceActivity extends PreferenceActivity {
+
+    private AppCompatDelegate mDelegate;
+    private SharedPreferences preferences;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getDelegate().installViewFactory();
+        getDelegate().onCreate(savedInstanceState);
+
+        // get preferences, here for testings, has to be in updateStatusActivity at the end
+        preferences = PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext());
+        //TODO GET FROM R.string.key_username
+        String username = preferences.getString("username", "DEFAULT");
+        String password = preferences.getString("password", "DEFAULT");
+
+        // Listener on preferences changings
+        SharedPreferences.OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
+                SharedPreferences.Editor editor = preferences.edit();
+                Toast toast = Toast.makeText(getApplicationContext(), "Settings have changed !", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        };
+        preferences.registerOnSharedPreferenceChangeListener(listener);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        getDelegate().onPostCreate(savedInstanceState);
+    }
+
+    public ActionBar getSupportActionBar() {
+        return getDelegate().getSupportActionBar();
+    }
+
+    public void setSupportActionBar(@Nullable Toolbar toolbar) {
+        getDelegate().setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public MenuInflater getMenuInflater() {
+        return getDelegate().getMenuInflater();
+    }
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        getDelegate().setContentView(layoutResID);
+    }
+
+    @Override
+    public void setContentView(View view) {
+        getDelegate().setContentView(view);
+    }
+
+    @Override
+    public void setContentView(View view, ViewGroup.LayoutParams params) {
+        getDelegate().setContentView(view, params);
+    }
+
+    @Override
+    public void addContentView(View view, ViewGroup.LayoutParams params) {
+        getDelegate().addContentView(view, params);
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        getDelegate().onPostResume();
+    }
+
+    @Override
+    protected void onTitleChanged(CharSequence title, int color) {
+        super.onTitleChanged(title, color);
+        getDelegate().setTitle(title);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        getDelegate().onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        getDelegate().onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        getDelegate().onDestroy();
+    }
+
+    public void invalidateOptionsMenu() {
+        getDelegate().invalidateOptionsMenu();
+    }
+
+    private AppCompatDelegate getDelegate() {
+        if (mDelegate == null) {
+            mDelegate = AppCompatDelegate.create(this, null);
+        }
+        return mDelegate;
+    }
+}
--- app/src/main/java/com/sar2016/olivier/alexandra/yamba/UpdateStatusActivity.java
+++ app/src/main/java/com/sar2016/olivier/alexandra/yamba/UpdateStatusActivity.java
@@ -1,7 +1,9 @@
 package com.sar2016.olivier.alexandra.yamba;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.StrictMode;
+import android.preference.PreferenceManager;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,21 +16,30 @@ public class UpdateStatusActivity extends AppCompatActivity {
     private Button button;
     private EditText textArea;
     private String recupText;
-
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG,"created");
+        Log.d(TAG, "created");
         setContentView(R.layout.activity_update_status);
-        button = (Button)findViewById(R.id.update_status_button);
-        textArea = (EditText)findViewById(R.id.update_status_editText);
+        button = (Button) findViewById(R.id.update_status_button);
+        textArea = (EditText) findViewById(R.id.update_status_editText);
         button.setOnClickListener(new UpdateStatusClickListener(textArea, recupText, this));
         int green = ResourcesCompat.getColor(getResources(), R.color.colorGreen, null);
         int orange = ResourcesCompat.getColor(getResources(), R.color.colorOrange, null);
         int red = ResourcesCompat.getColor(getResources(), R.color.colorRed, null);
         textArea.addTextChangedListener(new UpdateStatusTextWatcher(this.textArea, green, orange, red));
+
+       // preferences = PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext());
+
+
+
         startService(new Intent(getBaseContext(), GetNewStatusesService.class));
+    }
+
+    public SharedPreferences getPreferences() {
+        return this.preferences;
     }
 
 }
