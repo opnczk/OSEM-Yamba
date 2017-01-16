8d5e4a79f3f71be37c27f38acfd4e537884f7a15
opnczk <opanczuk@gmail.com>
2017-01-16 17:46:40 +0100

    Local changes before merge, basically on the timeline

---
+++ app/src/main/java/com/sar2016/olivier/alexandra/yamba/TweetAdapter.java
@@ -0,0 +1,10 @@
+package com.sar2016.olivier.alexandra.yamba;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by olivier on 16/01/17.
+ */
+
+public class TweetAdapter extends BaseAdapter {
+}
